5c8888cd7fee322e4e6797f5031329f1409bed3a
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-04 04:53:17 +0300

    Python programming

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"{number:d}")
-if number > 0:
-    number = number % 10
-if number < 0:
-    number = (number * -1) % 10
-print(f"{number:d}")
+if number % 10 > 5 and number > 0:
+    print(f"Last digit of {number:d} is {number % 10:d} and is greater than 5")
+if number % 10 < 6 and number % 10 != 0 and number > 0:
+    print(f"Last digit of {number:d} is {number % 10:d} and is less than 6 and not 0")
+if number % 10 == 0 and number > 0:
+    print(f"Last digit of {number:d} is {number % 10:d} and is 0")
+if number % 10 > 5 and number < 0:
+    print(f"Last digit of {number:d} is {(number * -1) % 10:d} and is greater than 5")
+if number % 10 < 6 and number != 0 and number < 0:
+    print(f"Last digit of {number:d} is {(number * -1) % 10:d} and is less than 6 and not 0")
+if number % 10 == 0 and number < 0:
+    print(f"Last digit of {number:d} is {(number * -1) % 10:d} and is 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 i = 97
 while i < 123:
-    print(chr(i), end='')
+    print("{}".format(chr(i)), end='')
     if i == 100 or i == 112:
         i = i + 1
     i = i + 1
