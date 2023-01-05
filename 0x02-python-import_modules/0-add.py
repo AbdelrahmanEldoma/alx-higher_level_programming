fb7553acbe08fe8e3713482522fbbc2e9a669949
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-06 02:38:31 +0300

    Python programming

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end="")
+
+def main():
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end="")
+
+
+if __name__ == "__main__":
+    main()
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import calculator_1
+import calculator_1
 
-a = 10
-b = 5
-print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)), end="")
-print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)), end="")
-print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)), end="")
-print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)), end="")
+
+def main():
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)), end="")
+    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)), end="")
+    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)), end="")
+    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)), end="")
+
+
+if __name__ == "__main__":
+    main()
