305c6c2ed545a790b64266021dfdd91809fd3240
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-06 03:53:31 +0300

    Python programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):
-    print("{:d} arguments:".format(len(sys.argv)))
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+
+def main():
+    for i in range(0, len(sys.argv) - 1):
+        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d}: {:s}".format(i + 1, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
