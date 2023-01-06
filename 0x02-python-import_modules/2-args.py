6b838355fff8ff339a7c3806c32cc6646860450d
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-06 04:17:38 +0300

    Python programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,22 @@ import sys
 
 
 def main():
-    if len(sys.argv) == 0:
-        print("{} arguments.".format(len(sys.argv)))
-    elif len(sys.argv) == 1:
-        print("{} argument:".format(len(sys.argv)))
-        for i in range(0, len(sys.argv)):
-            print("{}: {}".format(i + 1, sys.argv[i]))
-    else:
-        print("{} arguments:".format(len(sys.argv)))
-        for i in range(0, len(sys.argv)):
-            print("{}: {}".format(i + 1, sys.argv[i]))
+    for j in range(0, len(sys.argv)):
+        if len(sys.argv) == 0:
+            print("{:d} arguments.".format(len(sys.argv)), end="")
+        elif len(sys.argv) == 1:
+            print("{:d} argument:".format(len(sys.argv)))
+            for i in range(0, len(sys.argv)):
+                print("{:d}: {:s}".format(i + 1, sys.argv[i]), end="")
+        elif j < len(sys.argv) - 1:
+            print("{:d} arguments:".format(len(sys.argv)))
+            for i in range(0, len(sys.argv)):
+                print("{:d}: {:s}".format(i + 1, sys.argv[i]))
+        else:
+            print("{:d} arguments:".format(len(sys.argv)))
+            for i in range(0, len(sys.argv)):
+                print("{:d}: {:s}".format(i + 1, sys.argv[i]), end="")
+
 
 
 if __name__ == "__main__":
