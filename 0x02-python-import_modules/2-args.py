e367e78f7e2b40d595a3587a160f0ded81f96fdc
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-06 04:33:35 +0300

    Python programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,15 @@ import sys
 
 def main():
     if len(sys.argv) - 1 == 0:
-        print("{:d} arguments.".format(len(sys.argv) - 1), end="")
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) - 1 == 1:
         print("{:d} argument:".format(len(sys.argv)))
-        print("{:d}: {:s}".format(1, sys.argv[1]), end="")
+        print("{:d}: {:s}".format(1, sys.argv[1]))
     else:
         for j in range(1, len(sys.argv)):
             if j == len(sys.argv):
                 print("{:d} arguments:".format(len(sys.argv)))
-                print("{:d}: {:s}".format(j, sys.argv[j]), end="")
+                print("{:d}: {:s}".format(j, sys.argv[j]))
             else:
                 print("{:d} arguments:".format(len(sys.argv)))
                 print("{:d}: {:s}".format(j, sys.argv[j]))
