cf74e74b1589d50f7678009fcfbb8d0b7362bb7c
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-08 07:04:26 +0300

    Python programming

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        if i == len(my_list) - 1:
+            print("{:d}".format(my_list[i]), end="")
+        else:
+            print("{:d}".format(my_list[i]))
