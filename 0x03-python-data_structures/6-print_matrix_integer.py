3bce042d049b7886f415efa0bab0ae209eee1843
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-10 00:44:46 +0300

    Python programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for rows in range(len(matrix)):
-        for columns in range(len(matrix)):
-            if columns == len(matrix) - 1:
-                print("{:d}".format(matrix[rows][columns]))
-            else:
-                print("{:d}".format(matrix[rows][columns]), end=" ")
+    if matrix:
+        for rows in range(len(matrix)):
+            for columns in range(len(matrix)):
+                if columns == len(matrix) - 1:
+                    print("{:d}".format(matrix[rows][columns]))
+                else:
+                    print("{:d}".format(matrix[rows][columns]), end=" ")
