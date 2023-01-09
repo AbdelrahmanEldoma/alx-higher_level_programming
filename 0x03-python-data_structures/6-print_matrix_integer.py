7f0fcdda05e2e5bb016ac1df9c5dea339c5f6daa
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-10 01:09:08 +0300

    Python programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,4 +10,3 @@ def print_matrix_integer(matrix=[[]]):
                         print("{:d}".format(matrix[rows][columns]))
                     else:
                         print("{:d}".format(matrix[rows][columns]), end=" ")
-
