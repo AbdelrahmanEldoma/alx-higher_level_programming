477a5b7d5541ecb1bd28dc260eb50c1e4ba5c5ea
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2023-01-29 07:34:26 +0300

    Python programming

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str2 = ""
+    if n > len(str) or n < 0:
+        return str
+    else:
+        for i in str:
+            if i != str[n]:
+                str2 = str2 + i;
+        return str2
